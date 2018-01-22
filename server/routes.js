ce6148ab540e70a0e8d1aa67b3679fc8975d9474
OLANIYI TAWAKALT <olaniyitaiwo95@yahoo.com>
2018-01-22 15:24:57 +0100

    create amebo recipe (#2)

--- server/routes.js
+++ server/routes.js
@@ -3,4 +3,8 @@ module.exports = (app) => {
     app.get('/face/:from/:to', (req, res) => {
         require('./recipes/front')(req, res);
     });
+
+    app.get('/amebo/:from/:to', (req, res) => {
+        require('./recipes/amebo')(req, res);
+    });
 };
